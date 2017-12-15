87d5ad885aee30f5e385f63ebb6b760b055a2d80
and1helicopter <and1helicopter@mail.ru>
2017-12-15 16:30:06 +0300

    v0.0a0.08 +add scrollbar for left_menu +add dynamic style for button_server +add corect content for iec61850

--- iec61850_Client/Configurator/view/js/main.js
+++ iec61850_Client/Configurator/view/js/main.js
@@ -1,4 +1,12 @@
-var data = {
+class server61850 {
+    constructor(){
+        this.host = {};
+        this.port = {};
+        this.items = [];
+    }
+}
+
+data = {
     host:'localhost',
     port: 2404,
     maxQueue: 10,
@@ -7,18 +15,25 @@ var data = {
     servers61850:[]
 };
 
-class server61850 {
-    constructor(host, port){
-        this.host = host;
-        this.port = port;
-        this.items = [];
-    }
-}
+actual = {
+    index: 0
+};
+
+show = {
+    show61850: [],
+    show60870: false
+};
+
+map = {
+    data: data,
+    actual: actual,
+    show: show
+};
 
 Vue.component('app-iec61850',{
     template: '#iec61850',
     data: function () {
-        return data
+        return map
     },
     methods: {
 
@@ -27,18 +42,28 @@ Vue.component('app-iec61850',{
 
 Vue.component('app-header_left', {
     template: '#headerLeft',
-    props: ["show"],
     data: function () {
-        return data
+        return map
     },
     methods: {
         add_server61850: function () {
-            this.servers61850.push(new server61850('127.0.0.1', 102));
-            console.log(this.servers61850[length - 1]);
+            map.data.servers61850.push(new server61850());
+            map.data.servers61850[data.servers61850.length - 1].host = "127.0.0.1";
+            map.data.servers61850[data.servers61850.length - 1].port = 102;
+            map.show.show61850.push(false);
+
+            console.log(show.show61850[show.show61850.length - 1]);
         },
-        show_server61850: function (server) {
-            this.show = !this.show;
-            this.$emit('showstatuschange', this.show);
+        show_server61850: function (server, index) {
+            map.show.show61850[index] = !show.show61850[index];
+            for(let i = 0; i < map.show.show61850.length; i++){
+                if(i !== index){map.show.show61850[i] = false;}
+            }
+            this.$emit('showstatuschange', map.show.show61850[index]);
+            map.actual.index = index;
+            map.data.servers61850.sort();
+
+            console.log(index);
             console.log(server);
         }
     }
@@ -47,29 +72,30 @@ Vue.component('app-header_left', {
 Vue.component('app-iec60870', {
     template: '#iec60870',
     data: function () {
-        return data
+        return map
     },
     methods: {
         changeHost: function () {
-            console.log(data.host)
+            console.log(map.data.host)
         },
         changePort: function () {
-            console.log(data.port)
+            console.log(map.data.port)
         }
     }
 });
 
 Vue.component('app-header_right', {
     template: '#headerRight',
-    props: ["show"],
     data: function () {
-        return data
+        return map
     },
     methods: {
         show_on: function () {
-            this.show = !this.show;
-            this.$emit('showstatuschange', this.show);
-            console.log(this.show);
+            map.show.show60870 = !map.show.show60870;
+            this.$emit('showstatuschange',  map.show.show60870);
+            map.data.servers61850.sort();
+
+            console.log(map.show.show60870);
         }
     }
 });
@@ -84,7 +110,10 @@ new Vue({
         change_show60870: function (value) {
             this.show60870 = value;
             if(this.show60870){
-                this.show61850 = false
+                this.show61850 = false;
+                for(let i = 0; i < show.show61850.length; i++){
+                    show.show61850[i] = false;
+                }
             }
             console.log(this.show60870);
         },
