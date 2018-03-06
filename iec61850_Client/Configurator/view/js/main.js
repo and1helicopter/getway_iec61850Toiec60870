f2eb680f4dc0d006945c2cdcaa66b1f3c25b6b41
Denis <and1helicopter@mail.ru>
2018-03-06 11:36:34 +0300

    v0.0a0.17
    +Добавлено удаление ASDU

--- iec61850_Client/Configurator/view/js/main.js
+++ iec61850_Client/Configurator/view/js/main.js
@@ -1482,70 +1482,19 @@ Vue.component('app-iec61850',{
                 map.data.servers61850[index].itemsASDU[indexASDU].length = map.data.servers61850[index].itemsASDU[indexASDU].objects.length;
             }
         },
-        hasFirst: function() {
-            return this.current > 2;
-        },
-        hasLast: function() {
-            return this.rangeEnd < this.totalPages
-        },
-        hasPrev: function() {
-            console.log(this.current);
-            return this.current > 1;
-        },
-        hasNext: function() {
-            return this.current < this.totalPages
-        },
-        changePage: function(page) {
-            this.$emit('page-changed', page)
-        },
-        props: {
-            current: {
-                type: Number,
-                default: 0
-            },
-            total: {
-                type: Number,
-                default: 0
-            },
-            perPage: {
-                type: Number,
-                default: 9
-            },
-            pageRange: {
-                type: Number,
-                default: 2
+        remove_ASDU: function () {
+            let index = map.actual.index;
+            let indexASDU = this.ShowASDU[index].current;
+            map.data.servers61850[index].itemsASDU.splice(indexASDU, 1);
+            map.data.servers61850[index].itemsASDU.sort();
+            this.ShowASDU[index].total -= 1;
+            this.ShowASDU[index].show = false;
+            this.ShowASDU[indes].current = 0;
+            if(this.ShowASDU[index].total === 0) {
+                this.ShowASDU[index].show_left = false;
+                this.ShowASDU[index].show_right = false;
             }
-        },
-        watch: {
-            pages: function() {
-                let pages = [];
-
-                for(let i = this.rangeStart; i <= this.rangeEnd; i++) {
-                    pages.push(i)
-                }
-
-                return pages
-            },
-            rangeStart: function() {
-                let start = this.current - this.pageRange;
-
-                return (start > 0) ? start : 1
-            },
-            rangeEnd: function() {
-                let end = this.current + this.pageRange;
-
-                return (end < this.totalPages) ? end : this.totalPages
-            },
-            totalPages: function() {
-                return Math.ceil(this.total/this.perPage)
-            },
-            nextPage: function() {
-                return this.current + 1
-            },
-            prevPage: function() {
-                return this.current - 1
-            }
-        },
+        }
     }
 });
 
